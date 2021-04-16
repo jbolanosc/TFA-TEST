a3a1986a41e424faa80702a5fab018d433efeeb6
jbolanosc <josue.carit@outlook.com>
2021-04-15 20:12:26 -0600

    First test and page design read, waiting for tests

--- src/javascript-test/palindrome.js
+++ src/javascript-test/palindrome.js
@@ -1,1 +1,26 @@
 /* Export your NodeJS module from this file */
+exports.isPalindrome = (string) => {
+  let reverseString = "";
+
+  if (typeof string !== "string" || string == "") return false;
+
+  let formatString = string.replace(/[^a-zA-Z0-9\.]+/g, "");
+  formatString = formatString.toLowerCase();
+
+  for (let i = formatString.length - 1; i >= 0; i--) {
+    reverseString += formatString[i];
+  }
+
+  if (formatString === reverseString) return true;
+
+  return false;
+};
+
+exports.randomAsyncStringIsPalindrome = async (obj) => {
+  let string = await obj.get();
+  let result = false;
+
+  isPalindrome(string) ? (result = true) : (result = false);
+
+  return result;
+};
